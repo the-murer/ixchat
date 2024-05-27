2c839abee356816d536dd159b791126d2330d6b3
Bruno Murer <brunoburer@gmail.com>
2024-05-26 22:47:41 -0300

    feat: adicionar criacao de chats a partir da listagem

--- src/components/chats_list.tsx
+++ src/components/chats_list.tsx
@@ -3,45 +3,76 @@ import { Button, Card, Col, ListGroup } from "react-bootstrap";
 
 import { apiUrl } from "../contexts/auth_context";
 import axios from "axios";
+import { CircularProgress } from "@mui/material";
 
 const getUsers = async () => {
-    const response = await axios.get(`${apiUrl}/users`);
+    const response = await axios.get(`${apiUrl}/users/list`);
     return response.data;
 };
 
-function Chats() {
+const getChats = async (userId: string) => {
+    const response = await axios.get(`${apiUrl}/chats/${userId}`);
+    return response.data;
+};
+
+function Chats({ createChat }) {
+    const userId = localStorage.getItem("userId")
     const [chats, setChats] = useState([]);
     const [users, setUsers] = useState([]);
     const [showChats, setChatVisualization] = useState(true);
-
+    const [loading, setLoading] = useState(true);
     // console.log('USUARIOS', await getUsers())
     useEffect(() => {
-        setChats([{ id: "chat 1", users: ["1", "2"] }, { id: "chat 2", users: ["1", "3"] }, { id: "chat 3", users: ["1", "2", "3"] }]);
-        setUsers([{ id: '1', name: "user 1" }, { id: '2', name: "user 2" }, { id: '3', name: "user 3" }]);
+      const fetchData = async () => {
+        const response = await getUsers();
+        const responseChats = await getChats(userId);
+        console.log("🚀 ~ fetchData ~ responseChats:", responseChats)
+        setUsers(response.filter((user) => user._id !== userId));
+        setChats(responseChats);
+        setLoading(false);
+      };
+    
+      fetchData();
+    }, []);
+    
 
-    }, [])
-
+    if (loading) {
+      return (
+      <Col xs={3} style={{ display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <CircularProgress color="primary" size={50} thickness={5} />
+      </Col>
+      )
+    }
     return (
-      <Col xs={3}>
+      <Col xs={4}>
         <div className="sidebar-card">
           <div className="d-flex justify-content-between align-items-center">
-            <h6 className="mb-0">{showChats ? "Chats" : "Usuarios"}</h6>
+            <h4 className="mb-0">{showChats ? "Chats" : "Usuarios"}</h4>
             <Button size="sm" variant="outline-primary" onClick={() => setChatVisualization(!showChats)}>
               {showChats ? "Ver usuarios" : "Ver chats"}
             </Button>
           </div>
+          <hr />
           <ListGroup variant="flush" className="sidebar-list">
             {showChats ? chats.map((chat) => {
-              const label = users.filter((user) => chat.users.includes(user.id)).map((user) => user.name).join(", ");
+              const participantId = chat.participants.find((p) => p !== userId);
+              const label = users.find((user) => user._id === participantId)?.name;
+
               return (
-                <ListGroup.Item  key={chat.id} className="list-item" style={{ padding: "10px" }} >
-                  {label}
+                <ListGroup.Item  key={chat.id} className="list-item" style={{ display: "flex", padding: "10px", justifyContent: "space-between" }} >
+                  <h6>{label}</h6>
+                  <Button onClick={() => createChat(user._id, chats)} variant="outline-primary" size="sm" style={{ marginLeft: "10px" }}>
+                   Ver conversa
+                 </Button>
                 </ListGroup.Item>
                     );
               }) :
               users.map((user) => (
-               <ListGroup.Item key={user.id} className="list-item" style={{ padding: "10px" }}>
-                 {user.name}
+                <ListGroup.Item  key={user._id} className="list-item" style={{ display: "flex", padding: "10px", justifyContent: "space-between" }} >
+                  <h6>{user.name}</h6>
+                 <Button onClick={() => createChat(user._id, chats)} variant="outline-primary" size="sm" style={{ marginLeft: "10px" }}>
+                   Conversar
+                 </Button>
                </ListGroup.Item>
                ))}
           </ListGroup>
--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -5,9 +5,12 @@ import { useEffect, useState } from "react";
 import { Container } from "@mui/material";
 import Chats from "../components/chats_list";
 import MessageList from "../components/message_list";
+import { apiUrl } from "../contexts/auth_context";
+import axios from "axios";
 
 const ChatInterface = () => {
   const [messages, setMessages] = useState([]);
+  const [activeChat, setChat] = useState('');
   const userId = localStorage.getItem("userId");
 
   useEffect(() => {
@@ -16,12 +19,30 @@ const ChatInterface = () => {
     ]);
   }, []);
 
+  const createChat = async (receiverId, chats) => {
+    if (!userId) return;
+    if (userId && chats.length > 0) {
+      const exits = chats.find((chat) => chat.participants.includes(receiverId));
+      if (exits) {
+        setChat(exits._id);
+        return;
+      }
+    }
+    try {
+      const response =await axios.post(`${apiUrl}/chats/create`, { userId, receiverId });
+      const data = response.data;
+      
+      setChat(data._id);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Container style={{ height: "80vh", width: "200vw", backgroundColor: "white", padding: "20px", borderRadius: "10px" }}>
       <div style={{ display: "flex", flexDirection: "row" }}>
-        <Chats />
-        <div style={{ width: "10px", height: "100%", backgroundColor: "gray" }} />
-        <MessageList messages={messages} userId={userId} isWaiting={false} handleSendMessage={() => {}} />
+        <Chats createChat={createChat} />
+        <MessageList key={activeChat} messages={messages} userId={userId} isWaiting={false} handleSendMessage={() => {}} />
       </div>
     </Container>
   );
